b223d17dca98fbcdb17e133581386485f8e5ba8a
Selina-Knaepen <knaepen.selina@gmail.com>
2021-07-14 15:15:08 +0200

    Change format of the return value in getUser(user)

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -6,8 +6,8 @@ export class UserService {
   apiUrl = 'https://api.github.com/users';
 
   constructor(private http: HttpClient) { }
-  
+
   getUser(user: string) {
-    return this.http.get(this.apiUrl + "/" + user)
+    return this.http.get(`${this.apiUrl}/${user}`)
   }
 }
