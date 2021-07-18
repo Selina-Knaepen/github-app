3023a7fbb5ecceebd227fd527714f9cf5238d1b8
Selina-Knaepen <knaepen.selina@gmail.com>
2021-07-18 14:58:13 +0200

    Add UserSearchComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserService } from './services/user/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UserSearchComponent } from './user-search/user-search.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { UserSearchComponent } from './user-search/user-search.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     UserService
