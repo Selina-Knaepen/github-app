bded4a157f644e9e945099ccc0540c27e8f1c9e7
Selina-Knaepen <knaepen.selina@gmail.com>
2021-07-23 08:03:33 +0200

    Add unit test to CommitSearchComponent

--- src/app/components/commit-search/commit-search.component.spec.ts
+++ src/app/components/commit-search/commit-search.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Author } from 'src/app/models/author.model';
+import { Commit } from 'src/app/models/commit.model';
+import { CommitService } from 'src/app/services/commit/commit.service';
 
 import { CommitSearchComponent } from './commit-search.component';
 
 describe('CommitSearchComponent', () => {
   let component: CommitSearchComponent;
   let fixture: ComponentFixture<CommitSearchComponent>;
+  let commitServiceStub: Partial<CommitService> = {
+    getCommitsFromRepo: function() {
+      return of([new Commit("sha1", "Initial Commit", new Author("Selina", new Date("07-22-2021"))),
+        new Commit("sha2", "Add button", new Author("Selina", new Date("07-22-2021")))]);
+    },
+    getCommitByTerm: function() {
+      return of([new Commit("sha2", "Add button", new Author("Selina", new Date("07-22-2021")))]);
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommitSearchComponent ]
+      declarations: [ CommitSearchComponent ],
+      providers: [
+        { provide: CommitService, useValue: commitServiceStub }
+      ]
     })
     .compileComponents();
   });
@@ -19,9 +35,7 @@ describe('CommitSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  /*
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  */
 });
